40504f2c89a05a9debea39e15fc5ce384d68b11f
James McAvoy <jimcavoy@thetastream.com>
2025-01-15 13:07:53 -0500

    Handle XML Labels (#14)
    Handle the case if the 4774 are in XML encoded.
    Bump copyright year to 2025.
    Bump version number to 1.0.1.

--- Mp2tClipImpl/src/PmtProxy.cpp
+++ Mp2tClipImpl/src/PmtProxy.cpp
@@ -6,125 +6,129 @@
 
 namespace
 {
-	char TAG_HDMV[] = { (char)0x48, (char)0x44, (char)0x4D, (char)0x56, (char)0xFF, (char)0x1B, (char)0x44, (char)0x3F, 0 };
-	char TAG_HDPR[] = { (char)0x48, (char)0x44, (char)0x50, (char)0x52, (char)0xFF, (char)0x1B, (char)0x67, (char)0x3F, 0 };
+    char TAG_HDMV[] = { (char)0x48, (char)0x44, (char)0x4D, (char)0x56, (char)0xFF, (char)0x1B, (char)0x44, (char)0x3F, 0 };
+    char TAG_HDPR[] = { (char)0x48, (char)0x44, (char)0x50, (char)0x52, (char)0xFF, (char)0x1B, (char)0x67, (char)0x3F, 0 };
 }
 
 void PmtProxy::update(const lcss::ProgramMapTable& pmt)
 {
-	if (_version == pmt.version_number())
-	{
-		return;
-	}
+    if (_version == pmt.version_number())
+    {
+        return;
+    }
 
-	_version = pmt.version_number();
-	for (const auto& pe : pmt)
-	{
-		switch (pe.stream_type())
-		{
-		case 0x06:
-		case 0x15:
-		{
-			char value[255]{};
-			char format_identifier[5]{};
+    _version = pmt.version_number();
+    for (const auto& pe : pmt)
+    {
+        switch (pe.stream_type())
+        {
+        case 0x06:
+        case 0x15:
+        {
+            char value[255]{};
+            char format_identifier[5]{};
 
-			for (auto& desc : pe)
-			{
-				// registration_descriptor
-				if (desc.tag() == 0x05)
-				{
-					desc.value((BYTE*)value);
-					strncpy(format_identifier, value, 4);
-					break;
-				}
-				// metadata_descriptor
-				else if (desc.tag() == 0x26)
-				{
-					desc.value((BYTE*)value);
-					strncpy(format_identifier, value + 3, 4);
-					break;
-				}
-			}
+            for (auto& desc : pe)
+            {
+                // registration_descriptor
+                if (desc.tag() == 0x05)
+                {
+                    desc.value((BYTE*)value);
+                    strncpy(format_identifier, value, 4);
+                    break;
+                }
+                // metadata_descriptor
+                else if (desc.tag() == 0x26)
+                {
+                    desc.value((BYTE*)value);
+                    strncpy(format_identifier, value + 3, 4);
+                    break;
+                }
+            }
 
-			if (strcmp(format_identifier, "KLVA") == 0)
-			{
-				_pid2type.insert({ pe.pid(), STREAM_TYPE::KLVA });
-			}
-			else if (strcmp(format_identifier, "$EXI") == 0)
-			{
-				_pid2type.insert({ pe.pid(), STREAM_TYPE::$EXI });
-			}
-		}
-		break;
-		case 0x1B:
-		{
-			char value[255]{};
-			for (const auto& desc : pe)
-			{
-				// registration_descriptor
-				if (desc.tag() == 0x05)
-				{
-					desc.value((BYTE*)value);
-					break;
-				}
-			}
+            if (strcmp(format_identifier, "KLVA") == 0)
+            {
+                _pid2type.insert({ pe.pid(), STREAM_TYPE::KLVA });
+            }
+            else if (strcmp(format_identifier, "$EXI") == 0)
+            {
+                _pid2type.insert({ pe.pid(), STREAM_TYPE::$EXI });
+            }
+            else if (strcmp(format_identifier, "$XML") == 0)
+            {
+                _pid2type.insert({ pe.pid(), STREAM_TYPE::$XML });
+            }
+        }
+        break;
+        case 0x1B:
+        {
+            char value[255]{};
+            for (const auto& desc : pe)
+            {
+                // registration_descriptor
+                if (desc.tag() == 0x05)
+                {
+                    desc.value((BYTE*)value);
+                    break;
+                }
+            }
 
-			if (strcmp(value, TAG_HDMV) == 0 || strcmp(value, TAG_HDPR) == 0)
-			{
-				_pid2type.insert({ pe.pid(), STREAM_TYPE::HDMV });
-			}
-			else
-			{
-				_pid2type.insert({ pe.pid(), STREAM_TYPE::H264 });
-			}
-		}
-		break;
-		case 0x02:
-		{
-			_pid2type.insert({ pe.pid(), STREAM_TYPE::VIDEO });
-		}
-		break;
-		case 0x03:
-		case 0x04:
-		{
-			_pid2type.insert({ pe.pid(), STREAM_TYPE::AUDIO });
-		}
-		break;
-		case 0x24:
-		{
-			char value[255]{};
-			char format_identifier[5]{};
+            if (strcmp(value, TAG_HDMV) == 0 || strcmp(value, TAG_HDPR) == 0)
+            {
+                _pid2type.insert({ pe.pid(), STREAM_TYPE::HDMV });
+            }
+            else
+            {
+                _pid2type.insert({ pe.pid(), STREAM_TYPE::H264 });
+            }
+        }
+        break;
+        case 0x02:
+        {
+            _pid2type.insert({ pe.pid(), STREAM_TYPE::VIDEO });
+        }
+        break;
+        case 0x03:
+        case 0x04:
+        {
+            _pid2type.insert({ pe.pid(), STREAM_TYPE::AUDIO });
+        }
+        break;
+        case 0x24:
+        {
+            char value[255]{};
+            char format_identifier[5]{};
 
-			for (const auto& desc : pe)
-			{
-				// registration_descriptor
-				if (desc.tag() == 0x05)
-				{
-					desc.value((BYTE*)value);
-					strncpy(format_identifier, value, 4);
-					break;
-				}
-			}
+            for (const auto& desc : pe)
+            {
+                // registration_descriptor
+                if (desc.tag() == 0x05)
+                {
+                    desc.value((BYTE*)value);
+                    strncpy(format_identifier, value, 4);
+                    break;
+                }
+            }
 
-			if (strcmp(format_identifier, "HEVC") == 0)
-			{
-				_pid2type.insert({ pe.pid(), STREAM_TYPE::H265 });
-			}
-		}
-		break;
-		}
-	}
+            if (strcmp(format_identifier, "HEVC") == 0)
+            {
+                _pid2type.insert({ pe.pid(), STREAM_TYPE::H265 });
+            }
+        }
+        break;
+        }
+    }
 }
 
 PmtProxy::STREAM_TYPE PmtProxy::packetType(unsigned short pid)
 {
-	STREAM_TYPE type = STREAM_TYPE::UNKNOWN;
+    STREAM_TYPE type = STREAM_TYPE::UNKNOWN;
 
-	map_type::iterator it = _pid2type.find(pid);
-	if (it != _pid2type.end())
-	{
-		type = it->second;
-	}
+    map_type::iterator it = _pid2type.find(pid);
+    if (it != _pid2type.end())
+    {
+        type = it->second;
+    }
 
-	return type;
+    return type;
 }
